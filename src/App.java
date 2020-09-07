9a059aff8e3b78296431bc60a13c0691268a3359
Nando Villarreal <armando.villarealgmez@gmail.com>
2020-09-06 20:07:12 -0500

    first commit Java

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(final String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Que hago aqui");
+    }
+}
---
+++ src/moduleAutentication/Autentication.java
@@ -0,0 +1,8 @@
+package moduleAutentication;
+
+public class Autentication {
+    public static void main(final String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Que hago aqui");
+    }
+}
