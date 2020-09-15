f38cfe15326b707809d7be2ca5aa9b0a12294d4c
Nando Villarreal <armando.villarealgmez@gmail.com>
2020-09-15 15:20:46 -0500

    Cambiamos el desarrollo a spring

--- src/moduleAutentication/Autentication.java
+++
@@ -1,8 +0,0 @@
-package moduleAutentication;
-
-public class Autentication {
-    public static void main(final String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Que hago aqui");
-    }
-}
